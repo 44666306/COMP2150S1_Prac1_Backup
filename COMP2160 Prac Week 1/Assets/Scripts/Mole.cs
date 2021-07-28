dc7005781c856dd310dbb3383588b8ea918c4a81
44666306 <69493570+44666306@users.noreply.github.com>
2021-07-28 18:01:41 +1000

    Onclick events!

--- COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
+++ COMP2160 Prac Week 1/Assets/Scripts/Mole.cs
@@ -8,17 +8,31 @@ public class Mole : MonoBehaviour
 {
    public SpriteRenderer sprite;
 
-    public Color newColor = new Color(1, 0, 0);
+    public Color startColor = new Color(1, 0, 0);
+    public Color secondColor = new Color(0, 1, 0);
+
+    public float countDownTime = 5;
 
     // Start is called before the first frame update
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
+
+        //set the starting color
+        sprite.color = startColor;
     }
 
     // Update is called once per frame
     void Update()
     {
-        sprite.color = newColor;
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("mouse clicked");
+
+        //Change the color on mouse click
+        sprite.color = secondColor;
     }
 }
